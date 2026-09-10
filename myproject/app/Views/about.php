72ce799a7ffc6401a8ef82a20f366a851129fdb2
HEADS_KAPECODER <hea.delossantos.11Prog@gmail.com>
2026-09-10 17:09:01 +0800

    Completed Technical Assessment Project: POS, :D

--- myproject/app/Views/about.php
+++ myproject/app/Views/about.php
@@ -7,10 +7,10 @@
 </head>
 <body>
     <nav>
-        <a href="<?= base_url('/')?>">Home</a>
-        <a href="<?= base_url('about')?>">About</a>
-        <a href="<?= base_url('customers')?>">Customer Accounts</a>
-        <a href="<?= base_url('users')?>">User Accounts</a>
+        <a href="<?= base_url('/')?>">Home</a> |
+        <a href="<?= base_url('about')?>">About</a> |
+        <a href="<?= base_url('customers')?>">Customer Accounts</a> |
+        <a href="<?= base_url('users')?>">User Accounts</a> 
     </nav>
 
     <h1>About</h1>
--- myproject/app/Views/home.php
+++ myproject/app/Views/home.php
@@ -7,9 +7,9 @@
 </head>
 <body>
     <nav>
-        <a href="<?= base_url('/') ?>">Home</a>
-        <a href="<?= base_url('about') ?>">About</a>
-        <a href="<?= base_url('customers') ?>">Customer Accounts</a>
+        <a href="<?= base_url('/') ?>">Home</a> |
+        <a href="<?= base_url('about') ?>">About</a> |
+        <a href="<?= base_url('customers') ?>">Customer Accounts</a> |
         <a href="<?= base_url('users') ?>">User Accounts</a>
     </nav>
 
